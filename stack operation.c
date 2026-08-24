6e04343e7cfb986d95e4a2e265d72cf85693f024
Prem-nivas-E <premnivas.e10c29_bai28@mepcoeng.ac.in>
2026-08-24 16:36:03 +0530

    Build an Array With Stack Operations

---
+++ stack operation.c
@@ -0,0 +1,18 @@
+char ** buildArray(int* target, int targetSize, int n, int* returnSize) {
+    char** ans = (char**)malloc(sizeof(char*) *(2*n));
+    int sz=0;
+    int input = 1;
+      for (register int i = 0; i < targetSize; i++) {
+        while (target[i] > input) {
+            ans[sz++]="Push";
+            ans[sz++]="Pop";
+            input++;
+        }
+        ans[sz++]="Push";
+        input++;
+    }
+
+    *returnSize=sz;
+
+    return ans;
+}
